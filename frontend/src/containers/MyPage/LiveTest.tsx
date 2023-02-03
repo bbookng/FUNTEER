f7e4cd09c620bc417ecc3ca5de8958c9db2670e3
김승섭 <sub9707@naver.com>
2023-02-03 11:35:34 +0900

    opnVidu 수정
    Former-commit-id: 0380a237f1e6d66500f49138292502186792da89

--- frontend/src/containers/MyPage/LiveTest.tsx
+++ frontend/src/containers/MyPage/LiveTest.tsx
@@ -1,10 +1,14 @@
+import { OpenVidu } from 'openvidu-browser';
 import React from 'react';
+import VideoRoomComponent from '../../components/VideoRoomComponent';
 import styles from './LiveTest.module.scss';
 
 export function LiveTest() {
   return (
     <div className={styles.bodyContainer}>
-      <div className={styles.contentBox} />
+      <div className={styles.contentBox}>
+        <VideoRoomComponent />
+      </div>
     </div>
   );
 }
